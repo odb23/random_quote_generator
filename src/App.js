5abdeeb93067f0748301a004af09bfe25f06158d
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-04-20 23:20:57 +0100

    added images and app layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react"; 
+import React from "react";
 import './App.css';
+import axios from "axios";
 
 class App extends React.Component {
+    state = {
+        advice: "",
+    }
+
+    componentDidMount() {
+        this.fetchAdvice();
+    }
+
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                const { advice } = response.data.slip;
+                this.setState({ advice });
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    }
+
     render() {
+        const { advice } = this.state;
         return (
-            <h1>App</h1>
+            <div className="App">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                </div>
+            </div>
         );
     }
 }
+
+export default App;
