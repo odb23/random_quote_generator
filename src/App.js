8097852c820f98a0e3b0a04dd94d6fa94e7d7dbe
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-04-20 23:43:52 +0100

    added button

--- src/App.js
+++ src/App.js
@@ -28,10 +28,14 @@ class App extends React.Component {
             <div className="app">
                 <div className="card">
                     <h1 className="heading">{advice}</h1>
+                    <button className="button" onClick={this.fetchAdvice}>
+                        <span>GIVE ME ADVICE!</span>
+                    </button>
                 </div>
+
             </div>
         );
     }
 }
 
-export default App;
+export default App; 
